3a84134ce79a08ac17c4ed608a89dadf6dc66baa
Pratham Arora <prathamaroraa27@gmail.com>
2024-10-01 17:41:58 +0530

    added download button

--- Frontend/react-app/src/components/Blog.jsx
+++ Frontend/react-app/src/components/Blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IoMdDownload } from "react-icons/io";
 
 const Blog = ({ blog }) => {
   return (
@@ -6,6 +7,13 @@ const Blog = ({ blog }) => {
       <h3 className="text-xl font-bold text-gray-800 mb-2">{blog.title}</h3>
       <p className="text-gray-700 text-base mb-4">{blog.Content}</p>
       <h6 className="text-gray-500 text-sm">- {blog.Author}</h6>
+      
+      <div className="flex justify-center">
+      <button className="flex items-center px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+      <IoMdDownload />
+      </button>
+      </div>
+      
     </div>
   );
 };
